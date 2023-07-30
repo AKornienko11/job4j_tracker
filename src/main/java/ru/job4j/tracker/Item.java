aaa46e7baf60920048c2844aa87ea953c0bda481
AKornienko3 <Kornienko.srs@mail.ru>
2023-07-30 16:56:18 +0500

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,12 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
+
+    private LocalDateTime created;
     private int id;
     private String name;
-
-    public Item() {
-
-    }
 
     public Item(String name) {
         this.name = name;
@@ -15,6 +16,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Item() {
+
     }
 
     public int getId() {
@@ -31,5 +36,25 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime created = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = created.format(formatter);
+        System.out.println(currentDateTimeFormat);
+
+    }
+
+    class StartUI {
+        public static void main(String[] args) {
+            Item item = new Item();
+            LocalDateTime data = item.getCreated();
+            System.out.println(data);
+        }
     }
 }
