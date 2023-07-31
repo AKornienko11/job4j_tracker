f237f1457ab8757a5a873042193c3a41ab988d3b
AKornienko3 <Kornienko.srs@mail.ru>
2023-07-31 16:06:36 +0400

    2. Модель данных. [#174683 #433445]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -5,9 +5,12 @@ import java.util.Date;
 public class College {
     public static void main(String[] args) {
         Student learner = new Student();
-        learner.setName(" Kornienko A.A.");
-        learner.setGroup(" intern ");
+        learner.setName("Kornienko A.A.");
+        learner.setGroup("intern ");
         learner.setBeginning(new Date(1682845200000L));
-        System.out.println(learner.getName() + learner.getGroup() + learner.getBeginning());
+        System.out.println(learner.getName()
+                + System.lineSeparator() + learner.getGroup()
+                + System.lineSeparator()
+                + learner.getBeginning());
     }
 }
