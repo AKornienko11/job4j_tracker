dfe25ab04cceec7fbdfab78fd1c8278b08bfb947
AKornienko3 <Kornienko.srs@mail.ru>
2023-08-03 12:14:00 +0400

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,12 +2,15 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item {
-
-    private LocalDateTime created = LocalDateTime.now();
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
 
     public Item(String name) {
         this.name = name;
@@ -18,8 +21,21 @@ public class Item {
         this.name = name;
     }
 
-    public Item() {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(name, item.name);
+    }
 
+    @Override
+    public int hashCode() {
+        return 0;
     }
 
     public int getId() {
@@ -41,5 +57,4 @@ public class Item {
     public LocalDateTime getCreated() {
         return created;
     }
-
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,40 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item name = items[index];
+            if (name != null) {
+                rsl[count] = name;
+                count++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, count);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
+        }
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] names = new Item[items.length];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            Item name = items[index];
+            if (key.equals(name.getName())) {
+                names[count] = name;
+                count++;
+            }
+        }
+        names = Arrays.copyOf(names, count);
+        for (int index = 0; index < names.length; index++) {
+            System.out.println(names[index]);
+        }
+        return names;
     }
 
     public Item findById(int id) {
@@ -21,5 +57,10 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        System.out.println(tracker.findAll());
     }
 }
