d4993f31012508c4f0a4cc0438eab7195421060f
AKornienko3 <Kornienko.srs@mail.ru>
2023-08-11 00:10:14 +0400

    3.0. Реализация класса StartUI. Вывод меню [#500743]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,10 +1,39 @@
 package ru.job4j.tracker;
 
+import java.util.Scanner;
+
 public class StartUI {
-    public static void main(String[] args) {
-        Item user = new Item();
-        System.out.println(user);
+    public void init(Scanner scanner, Tracker tracker) {
+        Scanner input = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.println(" Select: ");
+            int select = Integer.parseInt(input.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+            System.out.println();
+        }
 
     }
 
+    private void showMenu() {
+        String[] menu = {"Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"};
+        System.out.println(" Menu: ");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + " ." + menu[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+
+    }
 }
