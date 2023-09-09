abad0cf188b4d70e461cc7c7cbf6292bb351adab
AKornienko3 <Kornienko.srs@mail.ru>
2023-09-09 21:56:50 +0500

    0.3. Тестирование исключений с junit [#219366]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -16,4 +16,12 @@ class FactTest {
                 });
         assertThat(exception.getMessage()).isEqualTo("N could not be less than 0");
     }
+
+    @Test
+    public void whenNotException() {
+        int n = 4;
+        int result = new Fact().calc(n);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
 }
