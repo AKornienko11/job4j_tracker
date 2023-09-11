28a61d07de0e58d15dd2296a00ee0aa6e0eacab7
AKornienko3 <Kornienko.srs@mail.ru>
2023-09-11 21:47:26 +0500

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                rsl = index;
+                System.out.println(rsl);
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("В массиве нет искомого элемента");
+        }
+       return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[]{"A", "B", "C"}, "D");
+        } catch (ElementNotFoundException e) {
+            System.out.println("Введите другой элемент");
+        }
+    }
+}
+
+
