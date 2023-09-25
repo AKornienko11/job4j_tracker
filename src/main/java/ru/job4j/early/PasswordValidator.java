90f68c1b7ea3d00e312978b3c0a22fee6546257f
AKornienko3 <Kornienko.srs@mail.ru>
2023-09-25 10:47:04 +0500

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -35,7 +35,9 @@ public class PasswordValidator {
             }
             if (!Character.isLetterOrDigit(symbol)) {
                 hasSpecial = true;
+                break;
             }
+
         }
         if (!hasUpCase) {
             throw new IllegalArgumentException(
