dac51ea1d91a306881358c7bc81c5357fe285186
AKornienko3 <Kornienko.srs@mail.ru>
2023-11-10 22:28:03 +0500

    1. Уникальные задачи. [#212653]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FullSearch {
+
+    public Set<String> extractNumber(List<Tasks> tasks) {
+        Set<String> result = new HashSet<>();
+        for (Tasks task : tasks) {
+            String rsl = task.getNumber();
+            result.add(rsl);
+        }
+        return result;
+    }
+}
