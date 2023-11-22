67b47b3d794cfd5ff1269c8abdd0ee23131ec991
AKornienko3 <Kornienko.srs@mail.ru>
2023-11-22 16:08:09 +0500

    0. Map, HashMap [#212652 #461456]

--- src/main/java/ru/job4j/collection/UsageMAp.java
+++ src/main/java/ru/job4j/collection/UsageMAp.java
@@ -5,7 +5,11 @@ import java.util.HashMap;
 public class UsageMAp {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("kornienko.srs@mail.ru", "Kornienko Aleksey ");
+        map.put("bozhok@yandex.ru", "Artem Bozhok");
+        map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
         map.put("kornienko.srs@mail.ru", "Kornienko Aleksey Andreevich");
+        map.put("bozhok@yandex.ru", "Artem Evgenevich Bozhok ");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
