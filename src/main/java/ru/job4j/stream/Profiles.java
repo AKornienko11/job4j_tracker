6288bb6cb75fa550607fa531faf227e3f23cab0b
AKornienko3 <Kornienko.srs@mail.ru>
2024-01-11 13:13:44 +0500

    6. Список адресов [#110062 #472325]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -5,7 +5,8 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profile) {
-        return profile.stream().map(p -> p.getAddress())
+        return profile.stream()
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 }
