0a5e3f66f9a32ba524c912399b87b6000c61591b
AKornienko3 <Kornienko.srs@mail.ru>
2024-01-14 18:34:16 +0500

    7. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -50,4 +50,11 @@ public class Address {
                 + ", apartment=" + apartment
                 + '}';
     }
+
+    Comparator<Address> comparator  = new Comparator<Address>() {
+        @Override
+        public int compare(Address o1, Address o2) {
+            return o1.getCity().compareTo(o2.getCity());
+        }
+    };
 }
