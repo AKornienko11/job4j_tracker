70ba6515c7f0b73bbd85dccc673f18369888dab9
AKornienko3 <Kornienko.srs@mail.ru>
2024-01-14 17:26:02 +0500

    7. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,12 +1,44 @@
 package ru.job4j.stream;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
-    public static List<Address> collect(List<Profile> profile) {
-        return profile.stream()
-                .map(Profile::getAddress)
-                .collect(Collectors.toList());
+
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream().
+                map(Profile::getAddress).
+                collect(Collectors.toList());
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream().
+                map(Profile::getAddress).
+                sorted(Comparator.comparing(Address::getCity)).
+                distinct().
+                collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        Address first = new Address("City1", "Street1", 1, 1);
+        Address second = new Address("City2", "Street2", 2, 2);
+        Address third = new Address("City3", "Street3", 3, 3);
+        Address fourth = new Address("City1", "Street1", 1, 1);
+        Address fifth = new Address("City2", "Street2", 2, 2);
+        Address seventh = new Address("City3", "Street3", 3, 3);
+        List<Profile> profiles = Arrays.asList(
+                new Profile(second),
+                new Profile(fourth),
+                new Profile(fifth),
+                new Profile(first),
+                new Profile(seventh),
+                new Profile(third)
+        );
+        List<Address> result = Profiles.collectSortWithoutDuplicate(profiles);
+        result.forEach(System.out::println);
+
     }
 }
