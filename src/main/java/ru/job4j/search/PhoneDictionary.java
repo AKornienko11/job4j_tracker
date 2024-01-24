8e6ceb7d3f8864549063c8243c6b2193dc3c5f75
AKornienko3 <Kornienko.srs@mail.ru>
2024-01-24 10:45:49 +0500

    2. Local-Variable Type Inference [#70626 #475952]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -12,10 +12,10 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         var result = new ArrayList<Person>();
-        Predicate<Person> phone = (var person) -> (person.getPhone().contains(key));
-        Predicate<Person> name = (var person) -> person.getName().contains(key);
-        Predicate<Person> address = (var person) -> person.getAddress().contains(key);
-        Predicate<Person> surName = (var person) -> person.getSurname().contains(key);
+        Predicate<Person> phone = (person) -> (person.getPhone().contains(key));
+        Predicate<Person> name = (person) -> person.getName().contains(key);
+        Predicate<Person> address = (person) -> person.getAddress().contains(key);
+        Predicate<Person> surName = (person) -> person.getSurname().contains(key);
         Predicate<Person> combine = phone.or(address).or(name).or(surName);
         for (var person : persons) {
             if (combine.test(person)) {
--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -6,13 +6,12 @@ public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
-       var index = 0;
+        var index = 0;
         for (var element : tasks) {
             if (element.getPriority() > task.getPriority()) {
                 break;
             }
             index++;
-
         }
         this.tasks.add(index, task);
     }
